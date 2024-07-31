533900a40a18c29b950562491a6855def81fa156
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-31 22:19:00 +0530

    queue using two stacks

---
+++ stack/QueueUsingTwoStacks.java
@@ -0,0 +1,52 @@
+package stack;
+
+import java.util.Stack;
+
+public class QueueUsingTwoStacks {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public QueueUsingTwoStacks() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void enqueue(int item) {
+        stack1.push(item);
+    }
+
+    public int dequeue() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
+        } else {
+            throw new IndexOutOfBoundsException("Queue is empty");
+        }
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public int size() {
+        return stack1.size() + stack2.size();
+    }
+
+    public static void main(String[] args) {
+        QueueUsingTwoStacks queue = new QueueUsingTwoStacks();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        System.out.println("Dequeue: " + queue.dequeue());
+        System.out.println("Dequeue: " + queue.dequeue());
+
+        queue.enqueue(4);
+        System.out.println("Size: " + queue.size());
+        System.out.println("Is empty? " + queue.isEmpty());
+    }
+}
